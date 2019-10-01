b0a5b782e704833e4f18fdb05f850cc539498c74
Logan <logan@atomicorp.com>
2019-09-30 23:47:24 -0400

    Added closed source copyright to each file in program.

--- src/include/CharacterPortrait.h
+++ src/include/CharacterPortrait.h
@@ -1,3 +1,11 @@
+/* *CopyRight 2018, Anthony Logan Mitchell, All rights reserved.
+   *Holy war is my personal programming project and all artwork
+   *and high level programming outside of SDL2 framework, openGL,
+   *and c++ api are my original work in progress.
+   *Unauthorized copying of this file, via any medium is strictly prohibited
+   *Proprietary and confidential
+   *Written by Logan Mitchell <loganmitchell2011@gmail.com>
+*/
 #ifndef CHARACTERPORTRAIT_H
 #define CHARACTERPORTRAIT_H
 #include "GeneralTexture.h"
--- src/include/Interaction.h
+++ src/include/Interaction.h
@@ -1,3 +1,11 @@
+/* *CopyRight 2018, Anthony Logan Mitchell, All rights reserved.
+   *Holy war is my personal programming project and all artwork
+   *and high level programming outside of SDL2 framework, openGL,
+   *and c++ api are my original work in progress.
+   *Unauthorized copying of this file, via any medium is strictly prohibited
+   *Proprietary and confidential
+   *Written by Logan Mitchell <loganmitchell2011@gmail.com>
+*/
 #ifndef INTERACTION_H
 #define INTERACTION_H
 #include <SDL.h>
